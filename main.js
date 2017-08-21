09dc6986552b73f5386c01f298b3f1db73e4d335
James <boydston.j@gmail.com>
2017-08-21 13:37:27 -0700

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+//###################################################################
+//        __
+//   ____/ /___  ____ ______
+//  / __  / __ \/ __ `/ ___/
+// / /_/ / /_/ / /_/ (__  )
+// \__,_/\____/\__, /____/
+//            /____/
+
+// Dog Constructor & Prototype
+function Dog () {
+  this.status
+  this.color
+  this.hungry
+  this.owner
+}
+
+var sadie = new Dog()
+var moonshine = new Dog()
+var atticus = new Dog()
+
+// Instances of Dog
+// Needed: sadie, moonshine, atticus
+
+//     __
+//    / /_  __  ______ ___  ____ _____  _____
+//   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
+//  / / / / /_/ / / / / / / /_/ / / / (__  )
+// /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
+
+// Human Constructor & Prototype
+function Human () {
+  this.pet
+  this.feed
+  this.cool
+}
+
+var mason = new Human
+var julia = new Human
+
+// Instances of Human
+// Needed: mason, julia
